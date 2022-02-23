4f9b652dd202c0dc6459c8992c095ddf9325b504
FabioDiCeglie <dcfabio96@gmail.com>
2022-02-23 14:07:58 +0100

    /me adjustment

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,8 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Space = require("../models").space;
+const Story = require("../models").story;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -21,7 +23,7 @@ router.post("/login", async (req, res, next) => {
 
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(400).send({
-        message: "User with that email not found or password incorrect"
+        message: "User with that email not found or password incorrect",
       });
     }
 
@@ -44,14 +46,19 @@ router.post("/signup", async (req, res) => {
     const newUser = await User.create({
       email,
       password: bcrypt.hashSync(password, SALT_ROUNDS),
-      name
+      name,
+    });
+    const newSpace = await Space.create({
+      title: newUser.name,
+      description: null,
+      userId: newUser.id,
     });
 
     delete newUser.dataValues["password"]; // don't send back the password hash
 
     const token = toJWT({ userId: newUser.id });
 
-    res.status(201).json({ token, ...newUser.dataValues });
+    res.status(201).json({ token, ...newUser.dataValues, newSpace });
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
       return res
@@ -67,9 +74,11 @@ router.post("/signup", async (req, res) => {
 // - get the users email & name using only their token
 // - checking if a token is (still) valid
 router.get("/me", authMiddleware, async (req, res) => {
+  const user = req.user;
+  const userSpace = await Space.findByPk(user, { include: [Story] });
   // don't send back the password hash
   delete req.user.dataValues["password"];
-  res.status(200).send({ ...req.user.dataValues });
+  res.status(200).send({ ...req.user.dataValues, userSpace });
 });
 
 module.exports = router;
--- routers/spaces.js
+++ routers/spaces.js
@@ -1,8 +1,10 @@
+const { default: Axios } = require("axios");
 const { Router } = require("express");
 const router = new Router();
 const Space = require("../models/").space;
+const Story = require("../models/").story;
 
-router.get("/", async (req, res) => {
+router.get("/spaces", async (req, res) => {
   try {
     const getAllSpaces = await Space.findAll();
 
@@ -11,4 +13,16 @@ router.get("/", async (req, res) => {
     return res.status(400).send("Something went wrong, sorry");
   }
 });
+
+router.get("/spaces/:id", async (req, res) => {
+  try {
+    const spaceId = req.params.id;
+
+    const getSpaceById = await Space.findByPk(spaceId, { include: [Story] });
+    res.status(200).send(getSpaceById);
+  } catch (erro) {
+    return res.status(400).send("The id is not correct!");
+  }
+});
+
 module.exports = router;
