0b82068939ea2d84111cbbeadeb152cb38744043
FabioDiCeglie <dcfabio96@gmail.com>
2022-02-23 21:58:45 +0100

    Back-end done

--- routers/spaces.js
+++ routers/spaces.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const router = new Router();
 const Space = require("../models/").space;
 const Story = require("../models/").story;
+const authMiddleware = require("../auth/middleware");
 
 router.get("/spaces", async (req, res) => {
   try {
@@ -20,6 +21,27 @@ router.get("/spaces/:id", async (req, res) => {
 
     const getSpaceById = await Space.findByPk(spaceId, { include: [Story] });
     res.status(200).send(getSpaceById);
+  } catch (erro) {
+    return res.status(400).send("The id is not correct!");
+  }
+});
+router.patch("/spaces/update/:id", authMiddleware, async (req, res) => {
+  try {
+    const spaceId = req.params.id;
+    const { title, description, backgroundColor, color } = req.body;
+
+    const getSpaceById = await Space.findByPk(spaceId);
+    if (!getSpaceById) {
+      res.status(400).send("This is not correct");
+    } else {
+      const updateSpace = await getSpaceById.update({
+        title,
+        description,
+        backgroundColor,
+        color,
+      });
+      res.status(200).send(updateSpace);
+    }
   } catch (erro) {
     return res.status(400).send("The id is not correct!");
   }
--- routers/story.js
+++ routers/story.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = new Router();
 const Space = require("../models/").space;
 const Story = require("../models/").story;
+const authMiddleware = require("../auth/middleware");
 
 router.delete("/:id", async (req, res, next) => {
   try {
@@ -17,6 +18,30 @@ router.delete("/:id", async (req, res, next) => {
     }
   } catch (err) {
     res.status(500).send(err.message);
+  }
+});
+
+router.post("/create", authMiddleware, async (req, res, next) => {
+  try {
+    console.log("request body", req.body);
+    const { name, content, imageUrl } = req.body;
+    if (!name || !content || !imageUrl) {
+      return res
+        .status(400)
+        .send("Please provide a name, content and imageUrl");
+    }
+    const story = await Story.create({
+      name,
+      content,
+      imageUrl,
+    });
+    if (!story) {
+      return res.status(404).send(`Can't create the story`);
+    }
+    res.status(200).send(story);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
