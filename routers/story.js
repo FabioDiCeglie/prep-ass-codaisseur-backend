37d7b94558845dafce8b38fa4367da13a10d0107
FabioDiCeglie <dcfabio96@gmail.com>
2022-02-26 02:25:57 +0100

    create story updated

--- routers/story.js
+++ routers/story.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = new Router();
 const Space = require("../models/").space;
 const Story = require("../models/").story;
+const Comment = require("../models").comment;
 const authMiddleware = require("../auth/middleware");
 
 router.delete("/:id", async (req, res, next) => {
@@ -30,18 +31,61 @@ router.post("/create", authMiddleware, async (req, res, next) => {
         .status(400)
         .send("Please provide a name, content and imageUrl");
     }
-    const story = await Story.create({
-      name,
-      content,
-      imageUrl,
-    });
-    if (!story) {
-      return res.status(404).send(`Can't create the story`);
+    const user = req.user;
+    const findSpace = await Space.findOne({ where: { userId: user.id } });
+    if (!findSpace) {
+      res.status(404).send("space id not found");
+    } else {
+      const newStory = await Story.create({
+        name,
+        content,
+        imageUrl,
+        spaceId: findSpace.id,
+      });
+      res.status(200).send(newStory);
     }
-    res.status(200).send(story);
   } catch (err) {
     console.log(err);
     next(err);
+  }
+});
+router.get("/comments/:id", async (req, res) => {
+  try {
+    const spaceId = req.params.id;
+
+    const story = await Space.findByPk(spaceId, {
+      include: { model: Story, include: [Comment] },
+    });
+    //const getAllComments = await story.findAll();
+    // where: { storyId: story.id },
+
+    res.status(201).json(story);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Something went wrong, sorry");
+  }
+});
+
+router.post("/comments/create/:id", async (req, res) => {
+  try {
+    const storyId = req.params.id;
+    const { email, comment } = req.body;
+
+    const findTheStory = await Story.findByPk(storyId);
+
+    if (!findTheStory) {
+      res.status(404).send("Story not found");
+    } else {
+      const createComment = await Comment.create({
+        email,
+        comment,
+        storyId: findTheStory.id,
+      });
+      res.status(201).json(createComment);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Something went wrong, sorry");
   }
 });
 
